917f2e732c132e9b192a700efc9e0fff93fb01c8
adra3n <srtc.kcgl@gmail.com>
2024-03-08 18:10:22 +0300

    styling for form and results

--- client/forms.py
+++ client/forms.py
@@ -2,6 +2,6 @@ from django import forms
 
 class ScrapeForm(forms.Form):
     CHOICES = [('id', 'ID'), ('class', 'Class')]
-    url = forms.URLField(label='URL')
-    element_type = forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect)
+    url = forms.URLField(label='Target URL')
+    element_type = forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect,label='Selector Type')
     element_name = forms.CharField(label='Element Name')
